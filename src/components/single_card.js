129e155d30635fd5ceeea8c849463cb72ed0dd0f
Nick <nickpereslugoff@gmail.com>
2018-08-04 17:48:53 -0400

    added Modals. Fixed modal view to display correct card

--- src/components/single_card.js
+++ src/components/single_card.js
@@ -1,51 +1,44 @@
 import React, { Component } from 'react';
 import theBack from '../images/TheBack.jpg'
+import { Button, Header, Image, Modal } from 'semantic-ui-react'
 
 class SingleCard extends Component {
 
   render() {
     return (
-
-
-
-      <div className="cardContainer">
-        <div className="flip-container" data-toggle="modal" data-target="#modalCard">
-        	<div className="flipper">
-            <div className="front">
-              <img className="cardImage" src={theBack} />
-            </div>
-        		<div className="back">
-        			<img className="cardImage" src={this.props.src} />
-        		</div>
-        	</div>
-        </div>
-      </div>
+        <Modal trigger={
+          <Card
+            key={this.props.key}
+            src={this.props.src}
+          />
+        }>
+          <Modal.Header>Select a Photo</Modal.Header>
+          <Modal.Content image>
+            <Modal.Description>
+              <Header>{this.props.title}</Header>
+              <p>Weve found the following gravatar image associated wit/ your e-mail address.</p>
+              <p>Is it okay to use this photo?</p>
+            </Modal.Description>
+          </Modal.Content>
+        </Modal>
     );
   }
 }
 
+const Card = ({ src, ...otherProps}) => (
+      <div className="cardContainer" {...otherProps}>
+        <div className="flip-container">
+          <div className="flipper">
+            <div className="front">
+              <img className="cardImage" src={theBack} />
+            </div>
+            <div className="back">
+              <img className="cardImage" src={src} />
+            </div>
+          </div>
+        </div>
+      </div>
+);
+
 
 export default SingleCard
-
-// <Modal
-//   open={this.state.open}
-//   basic
-//   size='large'
-// >
-//   <div className="dimmerView">
-//     <h1 className="dimmerTitle">{this.props.title}</h1>
-//     <img
-//       className="dimmerImage"
-//       src={this.props.src}
-//     />
-//     <p className="dimmerInfo">{this.props.message}</p>
-//     <div className="dimmerFooter">
-//       <Button
-//
-//         onClick={this.handleClose}
-//       >
-//         Close
-//       </Button>
-//     </div>
-//   </div>
-// </Modal>
